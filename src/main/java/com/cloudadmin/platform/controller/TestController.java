f31dccbc7bc8eeb0ddf2b6d741fd86ba6c02d899
crazywu <406437984@qq.com>
2024-12-15 18:54:27 +0800

    集成redis

--- src/main/java/com/cloudadmin/platform/controller/TestController.java
+++ src/main/java/com/cloudadmin/platform/controller/TestController.java
@@ -1,12 +1,21 @@
 package com.cloudadmin.platform.controller;
 
+import com.cloudadmin.platform.service.RedisService;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 public class TestController {
+
+    @Resource
+    private RedisService redisService;
+
     @GetMapping("/test")
     public String test() {
-        return "platform V2";
+        String value = redisService.getValue("hello");
+        return StringUtils.hasText(value)? value : "nil";
     }
 }
---
+++ src/main/java/com/cloudadmin/platform/service/RedisService.java
@@ -0,0 +1,24 @@
+package com.cloudadmin.platform.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedisService {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    public void setValue(String key, String value) {
+        stringRedisTemplate.opsForValue().set(key, value);
+    }
+
+    public String getValue(String key) {
+        return stringRedisTemplate.opsForValue().get(key);
+    }
+
+    public void deleteValue(String key) {
+        stringRedisTemplate.delete(key);
+    }
+}
